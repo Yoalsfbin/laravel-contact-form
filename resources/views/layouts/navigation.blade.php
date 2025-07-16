721270fdfa98f07566421b6644f0e20ddbbc8113
Yoshiki Yokoyama <y.yokoyfb@gmail.com>
2025-07-16 14:30:14 +0900

    管理者でログイン時ナビバーの色を変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,8 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+@php
+    $isAdmin = Auth::check() && Auth::user()->is_admin;
+@endphp
+
+<nav x-data="{ open: false }" class="{{ $isAdmin ? 'bg-blue-200 border-b border-blue-300 text-white' : 'bg-white border-b border-gray-100' }}">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
